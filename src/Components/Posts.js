7287561aca92e3c9664abf50eef54185fcfeaff7
TamaraFerr <tamarablacksmith@hotmail.com>
2023-05-05 18:53:43 -0300

    ajeitando os data-tests e functions

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,7 +1,42 @@
+import { useState } from "react"
+
 export default function Posts() {
+    const [save, setSave] = useState("bookmark-outline")
+    const [like, setLike] = useState("heart-outline")
+    const [likes, setLikes] = useState(101523)
+    const formatNumber = (number) => new Intl.NumberFormat("pt-BR").format(number) 
+    
+
+    function SavePost() {
+        if(save === "bookmark-outline" ) {
+        setSave("bookmark")
+        } else {
+        setSave("bookmark-outline")
+        }
+    } 
+
+    function LikePost() {
+        if(like === "heart-outline") {
+        setLike("heart")
+        setLikes(likes + 1)
+        } else {
+        setLike("heart-outline")
+        setLikes(likes - 1)
+        }
+    }
+
+    function LikeFoto() {
+        if (like === "heart-outline") {
+        setLike("heart")
+        setLikes(likes + 1)
+        } else {
+        setLikes(likes)
+        }
+    }
+
     return(
         <div class="posts">
-            <div class="post">
+            <div class="post" data-test="post">
                 <div class="topo">
                     <div class="usuario">
                         <img src="assets/img/meowed.svg" alt="meowed"/>
@@ -13,25 +48,25 @@ export default function Posts() {
                 </div>
 
                 <div class="conteudo">
-                    <img src="assets/img/gato-telefone.svg" alt="gato-telefone"/>
+                    <img src="assets/img/gato-telefone.svg" alt="gato-telefone" data-test="post-image" onClick={LikeFoto}/>
                 </div>
 
                 <div class="fundo">
                     <div class="acoes">
                         <div>
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name={like} style={{color: like === "heart-outline" ? "black" : "red"}} data-test="like-post" onClick={LikePost}></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
                         <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
+                            <ion-icon name={save} data-test="save-post" onClick={SavePost}></ion-icon>
                         </div>
                     </div>
 
                     <div class="curtidas">
                         <img src="assets/img/respondeai.svg" alt="respondeai"/>
                         <div class="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                            Curtido por <strong>respondeai</strong> e <strong data-test="likes-number">outras 101.523 pessoas</strong>
                         </div>
                     </div>
                 </div>
@@ -68,6 +103,42 @@ export default function Posts() {
                         <img src="assets/img/adorable_animals.svg" alt="adorable_animals"/>
                         <div class="texto">
                             Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="post">
+                <div class="topo">
+                    <div class="usuario">
+                        <img src="assets/img/meowed.svg" alt="meowed"/>
+                        meowed
+                    </div>
+                    <div class="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="conteudo">
+                    <img src="assets/img/gato-telefone.svg" alt="gato-telefone"/>
+                </div>
+
+                <div class="fundo">
+                    <div class="acoes">
+                        <div>
+                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                        <div>
+                            <ion-icon name="bookmark-outline"></ion-icon>
+                        </div>
+                    </div>
+
+                    <div class="curtidas">
+                        <img src="assets/img/respondeai.svg" alt="respondeai"/>
+                        <div class="texto">
+                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
                         </div>
                     </div>
                 </div>
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,11 +1,34 @@
+import { useState } from "react"
+
 export default function User() {
+    const [fotoDoUsuario, setFotoDoUsuario] = useState("assets/img/catanacomics.svg")
+    const [nomeUsuario, setNomeUsuario] = useState("catanacomics")
+  
+    function TrocaFoto() {
+        let novaFoto = prompt('Insira o link da nova foto!')
+        if(!novaFoto) {
+            novaFoto = fotoDoUsuario
+        } else {
+            setFotoDoUsuario(novaFoto)
+        }
+    }
+    function TrocaNome() {
+        let novoUsuario = prompt('Insira o novo nome!')
+        if(!novoUsuario) {
+            novoUsuario = nomeUsuario
+        } else {
+            setNomeUsuario(novoUsuario)
+        }
+    }
+
+
     return(
         <div class="usuario">
-                <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+                <img src={fotoDoUsuario} alt="imagem de perfil" data-test="profile-image" onClick={TrocaFoto}/>
                 <div class="texto">
                     <span>
-                        <strong>catanacomics</strong>
-                        <ion-icon name="pencil"></ion-icon>
+                        <strong data-test="name">{nomeUsuario}</strong>
+                        <ion-icon name="pencil" data-test="edit-name" onClick={TrocaNome}></ion-icon>
                     </span>
                 </div>
         </div>
