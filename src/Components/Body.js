ee2264e92c707e8ad261cd587a1ae71ccf08b35e
TamaraFerr <tamarablacksmith@hotmail.com>
2023-05-04 23:14:43 -0300

    separando os arquivos 100%

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,10 +2,18 @@ import NavBarr from "./NavBarr"
 import Stories from "./Stories"
 import Posts from "./Posts"
 import SideBarr from "./SideBarr"
-import Suggestions from "./Suggestions"
 
 export default function Body() {
     return(
-        <NavBarr />
+        <>
+            <NavBarr />
+            <div class="corpo">
+                <div class="esquerda">
+                    <Stories />
+                    <Posts />                    
+                </div>
+                <SideBarr />
+            </div>
+        </>
     )
 }
